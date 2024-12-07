25d7f0c217bb0b38e4446f5c03a8c682959727c9
raxeraditya <aditya1596k@gmail.com>
2024-12-07 10:02:05 +0530

    env update

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,5 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://awxdolqaoynidobsjpbc.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF3eGRvbHFhb3luaWRvYnNqcGJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMzMDQ3OTAsImV4cCI6MjA0ODg4MDc5MH0.7viTKJuD5l-gJ7Is175JJeSgesfw_kGeXV_r9hd7Va8';
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
